8ad06b733f316ab4f5ba73fec2029cecb93ab5fd
jahglow <jahglow@gmail.com>
2016-07-04 18:38:33 +0300

    added FixedHeader class for aggregated Table to set a fixed header on tables with .reportal-fixed-header

--- src/main.js
+++ src/main.js
@@ -5,3 +5,9 @@ import Confirmit from './lib/base.js';
 import poll from './commonFunctions.js';
 
 import FixedHeader from './aggregatedTable/FixedHeader.js';
+
+window.Reportal = {
+  Confirmit:Confirmit,
+  FixedHeader:FixedHeader,
+  poll:poll
+}
