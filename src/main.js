12cb7813848724ceed5cd6837b16c7820da13572
jahglow <jahglow@gmail.com>
2016-08-18 11:41:42 +0300

    Added Lazy Hierarchy

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
-//const Cf = require('./lib/base.js');
-
-import Confirmit from './lib/base.js';
-
-import Common from './commonFunctions.js';
 
 import HierarchyTable from './aggregatedTable/hierarchyTable.js'
 import FixedHeader from './aggregatedTable/FixedHeader.js';
 import AggregatedTable from './aggregatedTable/aggregatedTable.js';
+import LazyHierarchyFetch from './aggregatedTable/LazyHierarchyFetch.js'
 
 import DefaultConfig from './hitlist/hitlist.js';
 import Hitlist from './hitlist/hitlist.js';
 
 window.Reportal = {
-  Confirmit,
   FixedHeader,
-  Common,
   HierarchyTable,
   AggregatedTable,
+  LazyHierarchyFetch,
   Hitlist
 };
