cb785a4c0c11e55dfa2ee4a2df945c9ef13349ae
Ivan Pilyugin <jahglow@gmail.com>
2016-11-25 15:26:05 +0300

    changes in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-var styleBundle = require('./main.css');
+let styleBundle = require('./main.css');
+
 import TAhierarchy from 'r-table-hierarchy';
 import TableFloatingHeader from 'r-table-floating-header';
 import AggregatedTable from 'r-aggregated-table';
 import ReportalBase from 'r-reporal-base'
+import Hitlist from 'r-hitlist';
 
-import Hitlist from './hitlist/hitlist.js';
-
-window.Reportal = window.Reportal || {};
-ReportalBase.mixin(window.Reportal,{
-  Hitlist,
-  DefaultConfig
-});
+let Reportal = window.Reportal;
 
 export default Reportal
