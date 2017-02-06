b2197a9e95752a5d7ea873a0cc6979119d1143de
jahglow <jahglow@gmail.com>
2017-02-06 12:02:43 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let styleBundle = require('./main.css');
 import TAhierarchy from 'r-table-hierarchy';
 import TableFloatingHeader from 'r-table-floating-header';
 import AggregatedTable from 'r-aggregated-table';
-import ReportalBase from 'r-reporal-base'
+import ReportalBase from 'r-reportal-base'
 import Hitlist from 'r-hitlist';
 
 let Reportal = window.Reportal;
