8aae575ce56bd82131dce2812b08fc3df55bd639
son-tsss <marya-sokolova@yandex.ru>
2017-09-14 14:44:50 +0300

    correlation table helper

--- src/correlationChart/correlationView.js
+++ src/correlationChart/correlationView.js
@@ -17,12 +17,15 @@ export default class CorrelationView {
 
       document.querySelector(`#${this.tableContainer}`).classList.add('hidden');
       document.querySelector(`#${this.buttonsContainer}>#chart-switcher`).classList.add('selected');
+      document.querySelector(`#quadrant-table`).classList.add('hidden');
 
       document.querySelector(`#${this.buttonsContainer}>#table-switcher`).addEventListener('click', () => {
         document.querySelector(`#${this.tableContainer}`).classList.remove('hidden');
         document.querySelector(`#${this.chartContainer}`).classList.add('hidden');
         document.querySelector(`#${this.buttonsContainer}>#table-switcher`).classList.add('selected');
         document.querySelector(`#${this.buttonsContainer}>#chart-switcher`).classList.remove('selected');
+        document.querySelector(`#quadrant-table`).classList.remove('hidden');
+        document.querySelector(`#quadrant-chart`).classList.add('hidden');
       });
 
     document.querySelector(`#${this.buttonsContainer}>#chart-switcher`).addEventListener('click', () => {
@@ -30,6 +33,10 @@ export default class CorrelationView {
       document.querySelector(`#${this.chartContainer}`).classList.remove('hidden');
       document.querySelector(`#${this.buttonsContainer}>#table-switcher`).classList.remove('selected');
       document.querySelector(`#${this.buttonsContainer}>#chart-switcher`).classList.add('selected');
+      document.querySelector(`#quadrant-table`).classList.add('hidden');
+      document.querySelector(`#quadrant-chart`).classList.remove('hidden');
     });
+
+
   }
 }
